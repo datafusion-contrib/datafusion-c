da2eed9bfdc419a9e05684db532a2085a52f4d66
Ken Thompson <ken@3nigmatics.com>
2022-07-05 17:28:33 +0000

    Added go example

--- examples/sql.go
+++ examples/sql.go
@@ -42,57 +42,5 @@ func main() {
 		}
 
 	}
-	/*
-		try:
-		    error = (ctypes.c_void_p * 1)()
-		    try:
-		        data_frame = datafusion.df_session_context_sql(
-		            context, b'SELECT 1;', ctypes.pointer(error))
-		        if error[0] is not None:
-		            message = datafusion.df_error_get_message(error[0])
-		            print(f'failed to run SQL: {message.decode()}')
-		            exit(1)
-		        try:
-		            datafusion.df_data_frame_show(data_frame, ctypes.pointer(error));
-		            if error[0] is not None:
-		                message = datafusion.df_error_get_message(error[0])
-		                print('failed to show data frame: {message.decode()}')
-		                exit(1)
-		        finally:
-		            datafusion.df_data_frame_free(data_frame)
-		    finally:
-		        if error[0] is not None:
-		            datafusion.df_error_free(error[0])
-		finally:
-		    datafusion.df_session_context_free(context)
-	*/
-	//defer C.free(unsafe.Pointer(x))
-	/*
-		//doit()
-		//name := C
-		libpath := C.CString("libdatafusion.so")
 
-		defer C.free(unsafe.Pointer(libpath))
-
-		datafusionlib := C.dlopen(libpath, C.RTLD_LAZY)
-
-		//defer C.free(unsafe.Pointer(p))
-		//var imghandle int
-
-		if datafusionlib != nil {
-			//openimage := C.CString("openimage")
-			//defer C.free(unsafe.Pointer(openimage))
-			//fp := C.dlsym(imglib, openimage)
-			//if fp != nil {
-			//   fi := C.CString("fake.img")
-			//   defer C.free(unsafe.Pointer(fi))
-			//   imghandle = int(C.bridge_someFunc(C.someFunc(fp), fi))
-
-			//} else {
-			//    imghandle = myOpenImage("fake.img")
-			//}
-			//	C.dlclose(datafusionlib)
-		}
-		fmt.Println("Done")
-	*/
 }
